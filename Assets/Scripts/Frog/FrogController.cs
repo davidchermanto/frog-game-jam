78b16d407613a7c504668833c8bbf4f9e15dec5b
davidchermanto <davidchermanto@gmail.com>
2022-09-16 20:04:53 +1000

    Initial setup with world generation

---
+++ Assets/Scripts/Frog/FrogController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogController : MonoBehaviour
+{
+    private int currentX;
+    private int currentY;
+
+    private float currentSanity;
+    private float maxSanity;
+
+    private float jumpRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Generation/Tile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private int x;
+    private int y;
+
+    private Sprite sprite;
+
+    public void SetCoordinates(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+
+        AdjustPosition();
+    }
+
+    public void AdjustPosition()
+    {
+        transform.position = new Vector3(x * GlobalVars.tileSize, y * GlobalVars.tileSize, 0);
+    }
+
+    public Sprite GetSprite()
+    {
+        return sprite;
+    }
+
+    public int GetX()
+    {
+        return x;
+    }
+
+    public int GetY()
+    {
+        return y;
+    }
+}
